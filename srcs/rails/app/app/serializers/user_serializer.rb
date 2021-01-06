3ba739b2ed65f8eb3746bc91f4950f3f1900cbbb
fredrikalindh <52217940+fredrikalindh@users.noreply.github.com>
2021-01-06 09:21:41 +0100

    Removed extra friendships array

--- srcs/rails/app/app/serializers/user_serializer.rb
+++ srcs/rails/app/app/serializers/user_serializer.rb
@@ -2,7 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :login, :avatar_url, :wins, :losses, :relation_to_user, :online
   belongs_to :guild
   has_many :friends, serializer: FriendSerializer
-  has_many :friendships
   has_many :pending_friends
   has_many :pending_requests
   def relation_to_user
