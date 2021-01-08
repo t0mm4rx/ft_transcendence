0836a6b1a7a3c84e7b5a405c7e8386d8f2dff12f
Fredrika Lindh <fredrikalindh@gmail.com>
2021-01-08 20:07:33 +0100

    Merge with tom's work

--- srcs/backbone/front/src/Router.js
+++ srcs/backbone/front/src/Router.js
@@ -1,126 +1,128 @@
 /* Here we define the routes and pages of our application. */
 
-import Backbone from 'backbone';
-import Guilds from './pages/Guilds';
-import Home from './pages/Home';
-import Game from './pages/Game';
-import Tournaments from './pages/Tournaments';
-import Test from './pages/Test';
-import Auth from './pages/Auth';
-import NotificationsPanel from './views/NotificationsPanel';
-import ChatPanel from './views/ChatPanel';
-import UserMenu from './views/UserMenu';
-import Cookies from 'js-cookie';
-import $ from 'jquery';
-import User from './pages/User';
-import Livestream from './pages/Livestream';
+import Backbone from "backbone";
+import Guilds from "./pages/Guilds";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import Tournaments from "./pages/Tournaments";
+import Test from "./pages/Test";
+import Auth from "./pages/Auth";
+import NotificationsPanel from "./views/NotificationsPanel";
+import Chat from "./views/Channels";
+import UserMenu from "./views/UserMenu";
+import Cookies from "js-cookie";
+import $ from "jquery";
+import User from "./pages/User";
+import Livestream from "./pages/Livestream";
 
 export default Backbone.Router.extend({
-	routes: {
-		"": "home",
-		"home": "home",
-		"home/": "home",
-		"user/:id": "user",
-		"user/:id/": "user",
-		"guilds": "guilds",
-		"guilds/": "guilds",
-		"game": "game",
-		"game/": "game",
-		"livestream/:id": "livestream",
-		"livestream/:id/": "livestream",
-		"tournaments": "tournaments",
-		"tournaments/": "tournaments",
-		"test": "test",
-		"test/": "test",
-		"auth": "auth",
-		"auth/": "auth",
-		"token": "token",
-		"token/": "token",
-		":whatever": "notFound",
-		":whatever/": "notFound",
-	},
-	home: function () {
-		this.checkLogged();
-		this.showLayout();
-		new Home().render();
-	},
-	user: function (id) {
-		this.checkLogged();
-		this.showLayout();
-		new User({login: id}).render();
-	},
-	guilds: function () {
-		this.checkLogged();
-		this.showLayout();
-		new Guilds().render();
-	},
-	game: function () {
-		this.checkLogged();
-		this.showLayout();
-		new Game().render();
-	},
-	livestream: function () {
-		this.checkLogged();
-		this.showLayout();
-		new Livestream().render();
-	},
-	tournaments: function () {
-		this.checkLogged();
-		this.showLayout();
-		new Tournaments().render();
-	},
-	test: function () {
-		this.checkLogged();
-		this.showLayout();
-		new Test().render();
-	},
-	auth: function () {
-		if (!!Cookies.get('user')) {
-			window.location.hash = "/";
-		} else {
-			this.hideLayout();
-			new Auth().render();
-		}
-	},
-	token: function () {
-		$("body").html("You are being redirected...");
-		window.opener.postMessage({
-			params: window.location.href.split("?")[1]
-		}, "*");
-	},
-	notFound: function () {
-		window.location.hash = "/";
-	},
-	/* Check if the user is logged, if not we redirect him to the auth page */
-	checkLogged: function () {
-		const user = Cookies.get('user');
-		if (!user) {
-			window.location.hash = "auth/";
-		}
-	},
-	/* Show chat panel, notification panel and other elements only visible when logged */
-	showLayout: function () {
-		$("#menu").show();
-		if (!window.userMenuView)
-		{
-			window.userMenuView = new UserMenu({model: window.currentUser});
-			window.userMenuView.render();
-		}
-		if (!window.notificationPanelView)
-		{
-			window.notificationPanelView = new NotificationsPanel({model: window.notifications});
-			window.notificationPanelView.render();
-		}
-		if (!window.chatPanelView)
-		{
-			window.chatPanelView = new ChatPanel({model: window.chat});
-			window.chatPanelView.render();
-		}
-	},
-	hideLayout: function () {
-		$("#menu").hide();
-		if (window.chatPanelView) {
-			window.location.reload();
-		}
-	}
+  routes: {
+    "": "home",
+    home: "home",
+    "home/": "home",
+    "user/:id": "user",
+    "user/:id/": "user",
+    guilds: "guilds",
+    "guilds/": "guilds",
+    game: "game",
+    "game/": "game",
+    "livestream/:id": "livestream",
+    "livestream/:id/": "livestream",
+    tournaments: "tournaments",
+    "tournaments/": "tournaments",
+    test: "test",
+    "test/": "test",
+    auth: "auth",
+    "auth/": "auth",
+    token: "token",
+    "token/": "token",
+    ":whatever": "notFound",
+    ":whatever/": "notFound",
+  },
+  home: function () {
+    this.checkLogged();
+    this.showLayout();
+    new Home().render();
+  },
+  user: function (id) {
+    this.checkLogged();
+    this.showLayout();
+    new User({ login: id }).render();
+  },
+  guilds: function () {
+    this.checkLogged();
+    this.showLayout();
+    new Guilds().render();
+  },
+  game: function () {
+    this.checkLogged();
+    this.showLayout();
+    new Game().render();
+  },
+  livestream: function () {
+    this.checkLogged();
+    this.showLayout();
+    new Livestream().render();
+  },
+  tournaments: function () {
+    this.checkLogged();
+    this.showLayout();
+    new Tournaments().render();
+  },
+  test: function () {
+    this.checkLogged();
+    this.showLayout();
+    new Test().render();
+  },
+  auth: function () {
+    if (!!Cookies.get("user")) {
+      window.location.hash = "/";
+    } else {
+      this.hideLayout();
+      new Auth().render();
+    }
+  },
+  token: function () {
+    $("body").html("You are being redirected...");
+    window.opener.postMessage(
+      {
+        params: window.location.href.split("?")[1],
+      },
+      "*"
+    );
+  },
+  notFound: function () {
+    window.location.hash = "/";
+  },
+  /* Check if the user is logged, if not we redirect him to the auth page */
+  checkLogged: function () {
+    const user = Cookies.get("user");
+    if (!user) {
+      window.location.hash = "auth/";
+    }
+  },
+  /* Show chat panel, notification panel and other elements only visible when logged */
+  showLayout: function () {
+    $("#menu").show();
+    if (!window.userMenuView) {
+      window.userMenuView = new UserMenu({ model: window.currentUser });
+      window.userMenuView.render();
+    }
+    if (!window.notificationPanelView) {
+      window.notificationPanelView = new NotificationsPanel({
+        model: window.notifications,
+      });
+      window.notificationPanelView.render();
+    }
+    if (!window.chatPanelView) {
+      window.chatPanelView = new Chat({ model: window.chat });
+      window.chatPanelView.render();
+    }
+  },
+  hideLayout: function () {
+    $("#menu").hide();
+    if (window.chatPanelView) {
+      window.location.reload();
+    }
+  },
 });
---
+++ srcs/backbone/front/src/views/Channels.js
@@ -0,0 +1,83 @@
+/* Chat panel view. Like the notification panel, the frame template is used to open/close the panel.*/
+import Backbone from "backbone";
+import template from "../../templates/chat.html";
+import { FtSocket, FtSocketCollection } from "../models/FtSocket";
+import $ from "jquery";
+import * as _ from "underscore";
+import { ChannelMessages } from "../models/Channels";
+
+export default Backbone.View.extend({
+  initialize() {
+    this.listenTo(this.model, "sync", this.render);
+    this.listenTo(this.model, "add", this.render);
+    this.newSocket(this.model.channel_id);
+    this.model.fetch();
+  },
+  changeChannel(id) {
+    this.model.channel_id = id;
+    this.newSocket(this.model.channel_id);
+    this.model.fetch();
+  },
+  el: "#chat-chat",
+  events: {
+    "keyup #chat-input": "keyPressEventHandler",
+  },
+  model: ChannelMessages,
+  render() {
+    const template = _.template($("#tpl-chat-message").html());
+    $("#chat-messages").html("");
+    let lastUser = null;
+    const messages = this.model.models;
+    messages.forEach((message) => {
+      const messageJson = message.toJSON();
+      const username = message.get("username");
+      messageJson.sentByMe =
+        username == window.currentUser.attributes.username
+          ? "chat-message-container-me"
+          : "";
+      messageJson.sentByLast =
+        lastUser === username ? "chat-message-container-no-margin" : "";
+      lastUser = username;
+      const html = template(messageJson);
+      $("#chat-messages").append(html);
+    });
+    document.querySelector("#chat-messages").scrollTop = document.querySelector(
+      "#chat-messages"
+    ).scrollHeight;
+    return this;
+  },
+  keyPressEventHandler(event) {
+    if (event.keyCode == 13) {
+      if (event.target.id == "chat-input") this.newMessage();
+    }
+  },
+  newMessage() {
+    const input = $("#chat-input").val();
+    if (input == "") return;
+    $("#chat-input").val("");
+    $.ajax({
+      url: `http://localhost:3000/api/channels/${this.model.channel_id}/messages/`,
+      type: "POST",
+      data: `body=${input}`,
+    });
+  },
+  newSocket(channel_id) {
+    this.ftsocket = new FtSocket({
+      id: channel_id,
+      channel: "MessageChannel",
+    });
+    this.ftsocket.socket.onmessage = (event) => {
+      const event_res = event.data;
+      const msg = JSON.parse(event_res);
+      // Ignores pings.
+      if (msg.type === "ping") return;
+      if (msg.message) {
+        this.model.add({
+          username: msg.message.login,
+          body: msg.message.message,
+          date: msg.message.date,
+        });
+      }
+    };
+  },
+});
