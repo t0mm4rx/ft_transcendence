79bd9efe2b3570d6e5a6d8427e8bda9fe4b628f0
Mathis <Mathis@MacBook-Pro-de-Grosjean-2.local>
2021-01-12 17:57:55 +0100

    clean

--- srcs/rails/app/app/serializers/user_serializer.rb
+++ srcs/rails/app/app/serializers/user_serializer.rb
@@ -1,9 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
   attributes :id, :username, :login, :avatar_url, :wins, :losses, :relation_to_user, :online, :tfa, :otp_secret_key
-=======
-  attributes :id, :username, :login, :avatar_url, :wins, :losses, :relation_to_user, :online, :tfa, :admin
->>>>>>> e548e733e9c9032c9b26620a0506e7d6a351e997
+
   belongs_to :guild
   has_many :friends, serializer: FriendSerializer
   has_many :pending_friends
