073b8e1dc628dcab70d4f44e783cd3b4c5453fb5
Fredrika Lindh <fredrikalindh@gmail.com>
2021-02-25 14:03:02 -0600

    secret key = xxx

--- srcs/rails/app/config/environments/production.rb
+++ srcs/rails/app/config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -111,5 +112,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_cable.disable_request_forgery_protection = true
+
+  config.secret_key_base = "XXXXX"
 
 end
